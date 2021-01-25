6305507048f7f02fdee3e16db2d69560d70890c3
Jake Pressman <jpress7639@gmail.com>
2021-01-25 11:09:53 -0500

    fixed email link

--- client/src/Layout/Footer/Footer.jsx
+++ client/src/Layout/Footer/Footer.jsx
@@ -11,7 +11,7 @@ class Footer extends Component{
                     <li><a href="https://www.youtube.com/channel/UCNI5psXqpTEYpFEMZflXfaA/videos" style={{textDecoration: "none", color: "black"}}><img className="icon" src="https://res.cloudinary.com/dkhiieq9p/image/upload/v1611096284/Youtube-Splash-Icon-PNG-715x715_imzvmr.png"></img></a></li>
                     <li><a href="https://www.instagram.com/samanthalcapell/" style={{textDecoration: "none", color: "black"}}><img className="icon" src="https://res.cloudinary.com/dkhiieq9p/image/upload/v1611096286/res-console.cloudinary_smwos5.png"></img></a></li>
                     <li><a href="https://www.facebook.com/capellsamantha" style={{textDecoration: "none", color: "black"}}><img className="icon" src="https://res.cloudinary.com/dkhiieq9p/image/upload/v1611096289/res-console.cloudinary-1_b5dh0b.png"></img></a></li>
-                    <li><a href="samanthacapell@gmail.com" style={{textDecoration: "none", color: "black"}}><img className="icon" src="https://res.cloudinary.com/dkhiieq9p/image/upload/v1611096247/kisspng-email-computer-icons-logo-clip-art-computer-e-mail-cliparts-5aadd733460724.5480821115213422592868_h3exwi.png"></img></a></li>
+                    <li><a href="mailto:samanthacapell@gmail.com" style={{textDecoration: "none", color: "black"}}><img className="icon" src="https://res.cloudinary.com/dkhiieq9p/image/upload/v1611096247/kisspng-email-computer-icons-logo-clip-art-computer-e-mail-cliparts-5aadd733460724.5480821115213422592868_h3exwi.png"></img></a></li>
                 </ul>
             </nav>
             </>
