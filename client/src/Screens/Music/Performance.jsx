1fc1aca57fb15352f8241d60f60c114805819d39
Jake Pressman <jpress7639@gmail.com>
2021-10-25 15:02:14 -0400

    integrated bandsintown

--- client/src/Screens/Music/Performance.jsx
+++ client/src/Screens/Music/Performance.jsx
@@ -3,17 +3,38 @@ import { Component } from "react"
 import Layout from "../../Layout/Layout"
 import "./Performance.css"
 
-class Tour extends Component{
+class Tour extends Component {
+
+    componentDidMount() {
+        const script = document.createElement("script");
+
+        script.src = "https://widget.bandsintown.com/main.min.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+    }
+
     render() {
-    return(
-        <body className="tourpic">
-        <Layout>
-        <div className="tour">
-        <h2>There are no current tour dates. Stay tuned for future performances!</h2>
-        </div>
-        </Layout>
-        </body>
-         )
+        return (
+            <body className="tourpic">
+                <Layout>
+
+                    <a class="bit-widget-initializer"
+                        data-font=""
+                        data-language="en"
+                        data-display-details="true"
+                        data-text-color="#e7fae9"
+                        data-background-color="rgba(82, 136, 82, 0.01)"
+                        data-separator-color="#5E705E"
+                        data-popup-background-color="#ffffff"
+                        data-link-color="#B8F5B8"
+                        data-link-text-color="#25352B"
+                        data-artist-name="Samantha Louise"
+                    ></a>
+
+                </Layout>
+            </body>
+        )
     }
 }
 
